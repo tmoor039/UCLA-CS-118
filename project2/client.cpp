fd53cefc0bedbababb661c5734b1194b759717dc
Omar Ozgur <oozgur217@gmail.com>
2016-11-13 16:23:17 -0800

    Added client retransmission

--- project2/client.cpp
+++ project2/client.cpp
@@ -5,16 +5,28 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <server-host-or-ip> <port-number>\n", argv[0]);
-        exit(1);
-    }
-    string serverHost = string(argv[1]);
-    int serverPort = atoi(argv[2]);
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <server-host-or-ip> <port-number>\n", argv[0]);
+    exit(1);
+  }
+  string serverHost = string(argv[1]);
+  int serverPort = atoi(argv[2]);
 
-    UdpClient udpClient(serverHost, serverPort);
-    udpClient.set_timeout(0, 500000);
+  UdpClient udpClient(serverHost, serverPort);
+
+  // Set recv timeout
+  udpClient.set_timeout(0, 500000);
+
+  // Create and send packet
+  udpClient.set_send_buf("Message from client\n");
+  udpClient.send_packet();
+
+  // Retransmit in the case of a timeout
+  while (udpClient.receive_packet() < 0) {
+    fprintf(stderr, "The packet timed out. Retransmitting\n");
     udpClient.set_send_buf("Message from client\n");
     udpClient.send_packet();
-    udpClient.receive_packet();
+  }
+
+  return 0;
 }
--- project2/server.cpp
+++ project2/server.cpp
@@ -11,19 +11,19 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <port-number> <file-name>\n", argv[0]);
-        exit(1);
-    }
-    int port = atoi(argv[1]);
-    
-    sockaddr_storage peerAddr;
-    socklen_t peerAddrLen = sizeof(sockaddr_storage);
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <port-number> <file-name>\n", argv[0]);
+    exit(1);
+  }
+  int port = atoi(argv[1]);
 
-    UdpServer udpServer(port);
-    udpServer.receive_packet((sockaddr *) &peerAddr, &peerAddrLen);
-    udpServer.set_send_buf("Message from server\n");
-    //udpServer.send_packet((sockaddr *) &peerAddr, peerAddrLen);
+  sockaddr_storage peerAddr;
+  socklen_t peerAddrLen = sizeof(sockaddr_storage);
 
-    return 0;
+  UdpServer udpServer(port);
+  udpServer.receive_packet((sockaddr *) &peerAddr, &peerAddrLen);
+  udpServer.set_send_buf("Message from server\n");
+  udpServer.send_packet((sockaddr *) &peerAddr, peerAddrLen);
+
+  return 0;
 }
