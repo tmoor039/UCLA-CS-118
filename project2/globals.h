2dab7dbbe09883c7b3364c8c7a1937c58f1445f9
jcherianucla <jcherian@ucla.edu>
2016-11-13 18:59:54 -0800

    Handshake on server side

--- project2/globals.h
+++ project2/globals.h
@@ -11,6 +11,6 @@
 #define MSS 1032 // This is Packet + Header
 #define MAX_SEQ 30720 // Reset SEQ back to 0 after exceeding
 #define SSTHRESH 15360 // Also start value for receiver window size
-#define RTO 500	// In ms
+#define RTO 500000 // In ms
 
 #endif // GLOBALS_H
--- project2/server.cpp
+++ project2/server.cpp
@@ -1,48 +1,82 @@
 #include <iostream>
 #include "Udp.h"                                                                   
+#include "tcp.h"
 #include <sys/types.h>                                                             
 #include <sys/socket.h>                                                            
 #include <netdb.h>                                                                 
 #include <arpa/inet.h>                                                             
 #include <unistd.h>                                                                
-#include "globals.h"                                                               
 #include <stdlib.h>
 
 using namespace std;
 
 bool TcpSendData(UdpServer &udpServer, string fileName) {
-  return 0;
+	sleep(500);
+	return 0;
 }
 
 bool TcpHandshake(UdpServer &udpServer) {
-  sockaddr_storage peerAddr;
-  socklen_t peerAddrLen = sizeof(sockaddr_storage);
+	uint8_t* encodedPacket;
+	uint16_t seq, ack;
+	sockaddr_storage peerAddr;
+	socklen_t peerAddrLen = sizeof(sockaddr_storage);
 
-  udpServer.receive_packet((sockaddr *) &peerAddr, &peerAddrLen);
-  udpServer.set_send_buf("Message from server\n");
-  udpServer.send_packet((sockaddr *) &peerAddr, peerAddrLen);
-  sleep(500);
-  return 0;
+	// Initial infinite timeout
+	udpServer.set_timeout(0, 0);
+
+	while(udpServer.receive_packet((sockaddr *) &peerAddr, &peerAddrLen) < 0)
+	{
+		// Just keep trying
+	}
+	// Get the information about the SYN Packet from client
+	TCP_Packet receivedPacket((uint8_t*)udpServer.getRecvPacket());
+	ack = receivedPacket.getHeader().fields[ACK];
+	seq = receivedPacket.getHeader().fields[SEQ];
+	fprintf(stdout, "Receiving packet %hu\n", ack);
+
+	udpServer.set_timeout(0, RTO);
+
+	// Send SYN-ACK
+	fprintf(stdout, "Sending packet %d %d %d SYN\n", seq+1, PACKET_SIZE, SSTHRESH); 
+	TCP_Packet sendPacket(rand() % MAX_SEQ + 1, seq + 1, PACKET_SIZE, 1, 1, 0);
+	encodedPacket = sendPacket.encode();
+	// Set the sender info to the encoded information
+	udpServer.set_send_buf(string((char *)encodedPacket));
+	udpServer.send_packet((sockaddr *) &peerAddr, peerAddrLen);
+	// Retransmit if failure
+	while(udpServer.receive_packet((sockaddr *) &peerAddr, &peerAddrLen) < 0)
+	{
+		fprintf(stdout, "Sending packet %d %d %d Retransmission SYN\n", seq+1, PACKET_SIZE, SSTHRESH); 
+		udpServer.send_packet((sockaddr *) &peerAddr, peerAddrLen);
+	}
+	// Avoid Memory Leak
+	delete encodedPacket;
+	// Receive SYN ACK from client, to begin sending data
+	TCP_Packet received_syn_ack((uint8_t*)udpServer.getRecvPacket());
+	ack = received_syn_ack.getHeader().fields[ACK];
+	fprintf(stdout, "Receiving packet %hu\n", ack);
+	return 0;
 }
 
 int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    fprintf(stderr, "Usage: %s <port-number> <file-name>\n", argv[0]);
-    exit(1);
-  }
-  int port = atoi(argv[1]);
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <port-number> <file-name>\n", argv[0]);
+		exit(1);
+	}
+	int port = atoi(argv[1]);
+	string filename = argv[2];
 
-  UdpServer udpServer(port);
+	UdpServer udpServer(port);
 
-  if (TcpHandshake(udpServer) != 0) {
-    fprintf(stderr, "The TCP handshake failed\n");
-    exit(1);
-  }
+	if (TcpHandshake(udpServer) != 0) {
+		fprintf(stderr, "The TCP handshake failed\n");
+		exit(1);
+	}
 
-  if (TcpSendData(udpServer, argv[2]) != 0) {
-    fprintf(stderr, "Failed to send data\n");
-    exit(1);
-  }
+	if (TcpSendData(udpServer, filename) != 0) {
+		fprintf(stderr, "Failed to send data\n");
+		exit(1);
+	}
 
-  return 0;
+	return 0;
 }
