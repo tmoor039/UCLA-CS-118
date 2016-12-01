3c819adafd61a9c57e399b6a57f7628e7bec1214
jcherianucla <jcherian@ucla.edu>
2016-11-30 16:05:12 -0800

    Added some more macros and remove some redundancies

--- project2/globals.h
+++ project2/globals.h
@@ -2,11 +2,6 @@
 #define GLOBALS_H
 
 // All numbers are in terms of bytes unless otherwise stated
-#define SEQ 0
-#define ACK 1
-#define WIN 2
-#define FLAGS 3
-#define NUM_FIELDS 4
 #define PACKET_SIZE 1024 // Also Window Size
 #define PACKET_DATA_SIZE 1024
 #define HEADER_SIZE 8
@@ -15,14 +10,17 @@
 #define SSTHRESH 15360 // Also start value for receiver window size
 #define RTO 500 // In ms
 #define RECEIVED_FILE_NAME "received.data"
-
-// Congestion Control algorithm
-enum CCMode {
-    SS, // Slow Start
-    CA  // Congestion Avoidance
-};
+// Macros for TCP Header
+#define SEQ 0
+#define ACK 1
+#define WIN 2
+#define FLAGS 3
+#define NUM_FIELDS 4
+// Macros for congestion control
+#define SS 0
+#define CA 1
+#define FTR 2
 
 #define MIN_CWND 1024
-#define INITIAL_SSTHRESH 15360
 
 #endif // GLOBALS_H
